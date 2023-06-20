96a0ad1d0ee228746c0522bd676d45163c6cb804
fbhgjuqahghgehwaukwrg <94283918+fbhgjuqahghgehwaukwrg@users.noreply.github.com>
2023-06-20 19:34:21 +0300

    Add files via upload

---
+++ frontend/js/index.js
@@ -0,0 +1,62 @@
+document.addEventListener('mousemove', e =>{
+    Object.assign(document.documentElement, {
+        style: `
+        --move-x: ${(e.clientX - window.innerWidth / 2) * 0.005}deg;
+        --move-y: ${(e.clientY - window.innerHeight / 2) * -0.01}deg;
+        `
+    })
+})
+
+let gallery_button_right = document.getElementsByClassName("gallery__button__right")[0];
+let gallery_button_left = document.getElementsByClassName("gallery__button__left")[0];
+let gallery_cards = document.getElementsByClassName("gallery__cards")[0];
+console.log(gallery_cards)
+let card_index = 0
+let cards = []
+let card_images = [
+    "images\\axalotl.jpg",
+    "images\\bear.jpg",
+    "images\\bee.jpg",
+    "images\\cat.jpg",
+    "images\\chicken.jpg",
+    "images\\dolphin.jpg",
+    "images\\parrot.png",
+    "images\\sth.jpg",
+    "images\\wolf.jpg"
+]
+for (let i = 0; i < 3; i++) {
+    add_card(card_images[i], "right");
+}
+function add_card(image, side) {
+    let new_card = document.createElement("img");
+    new_card.src = image;
+    new_card.classList.add('gallery__card__pic');
+    
+    if (side == "left") {
+        cards.unshift(new_card);
+        gallery_cards.prepend(new_card);
+    }
+    else {
+        cards.push(new_card);
+        gallery_cards.append(new_card);
+    }
+}
+gallery_button_right.addEventListener("click", event => {
+    gallery_cards.removeChild(cards[0]);
+    cards.shift()
+    card_index++;
+    if (card_index > card_images.length - 1) {
+        card_index = 0
+    }
+    add_card(card_images[(card_index + 2) % card_images.length], "right")
+})
+
+gallery_button_left.addEventListener("click", event => {
+    gallery_cards.removeChild(cards[2]);
+    cards.pop()
+    card_index--;
+    if (card_index < 0) {
+        card_index =  card_images.length - 1
+    }
+    add_card(card_images[card_index], "left")
+})
